635608668aca32c778b556cfdfbfcba5af865dba
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-22 21:12:46 +0800

    Finished the etch-a-sktech project
    Finished the drawing part of the script
    Added a color picker element
    Added reset buttons for the color and the drawing board
    Finished styling the entire page with the landing page theme

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const gridSize = 16;
+
+let selectedColor = "#000000";
 let gridPixel = 100 / gridSize;
 
-function buildGrid(gridSize) {
-    const gridWhole = document.querySelector(".board");
-    console.log(gridWhole);
-    
+function buildGrid(gridSize) {    
     for (let x = 0; x < gridSize; x++) {
         for (let y = 0; y < gridSize; y++) {
             const yCurr = document.createElement('div');
@@ -15,16 +14,46 @@ function buildGrid(gridSize) {
     }
 }
 
-function changeColor(e) {
-    if (this.style.backgroundColor === "white"){
-        this.style.backgroundColor = "black";
-    }else {
-        this.style.backgroundColor = "white";
-    }
+function startDrawing(e) {
+    pixels.forEach(pixel => pixel.addEventListener('mousemove', changeColor));
+    window.addEventListener('mouseup', stopDrawing);
 }
+
+function changeColor(e) {
+    this.style.backgroundColor = selectedColor;
+}
+
+function stopDrawing(e) {
+    pixels.forEach(pixel => pixel.removeEventListener('mousemove', changeColor));
+    window.removeEventListener('mouseup', stopDrawing);
+}
+
+function resetBoard() {
+    pixels.forEach(pixel => pixel.style.backgroundColor = "white");
+}
+
+function resetColor() {
+    selectedColor = "#000000";
+    colorPickerWrapper.style.backgroundColor = selectedColor;
+}
+
+function selectColor(e) {
+    selectedColor = this.value;
+    colorPickerWrapper.style.backgroundColor = selectedColor;
+}
+
+const gridWhole = document.querySelector(".board");
 
 buildGrid(gridSize);
 
 const pixels = document.querySelectorAll(".pixel");
+const resetBoardButton = document.querySelector("#reset-board");
+const resetColorButton = document.querySelector("#reset-color");
+const colorPicker = document.querySelector("#color-picker");
+const colorPickerWrapper = document.querySelector("#color-wrapper");
 
-pixels.forEach(pixel => pixel.addEventListener('click', changeColor));
+colorPicker.addEventListener('change', selectColor);
+
+window.addEventListener('mousedown', startDrawing);
+resetBoardButton.addEventListener('click', resetBoard);
+resetColorButton.addEventListener('click', resetColor);
